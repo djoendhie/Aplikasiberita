0adb04bdaa21cf1d6fd90aeb9807cb9bb6f3f9b5
Djoendhie <ihaandjoe68@Gmail.com>
2017-11-14 14:48:55 +0700

    qw

--- Aplikasiberita/Aplikasiberita/KategoriTableViewController.swift
+++ Aplikasiberita/Aplikasiberita/KategoriTableViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class KategoriTableViewController: UITableViewController {
 
+    var arrkategori = [[String:String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //memanggil data json menggunakan alamofire
+        Alamofire.request("http://localhost/Aplikasiberita/index.php/api/getKategori").responseJSON { (response) in
+            
+            // check responsse
+            
+            if response.result.isSuccess {
+                
+                //kalau response sucess kita amnbil json
+                let json = JSON(response.result.value as Any)
+                // get json array dari jason d atas
+                self.arrkategori = json["data"].arrayObject as! [[String : String]]
+                //check d log
+                //print self attar berita
+                //chect jumlah array
+                if self.arrkategori.count > 0 {
+                    
+                    // refresh table view
+                    self.tableView.reloadData()
+                }
+            }
+            else{
+                
+                print("error server")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,23 +53,29 @@ class KategoriTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return arrkategori.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellKategori", for: indexPath) as! KategoriTableViewCell
 
-        // Configure the cell...
-
+        var serverid = arrkategori[indexPath.row]
+        
+        //print server id
+        var id_kategori = serverid["id_kategori"]
+        let kategori = serverid["nama_kategori"]
+        
+        //pindah ke label
+        cell.labelKat.text = kategori
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
